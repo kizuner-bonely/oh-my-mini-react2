3596656700734b797d60f11890e360a2221bd96d
kizuner-bonely <1003306162@qq.com>
2022-12-04 15:52:44 +0800

    feat: complete basic rollup config

---
+++ scripts/rollup/react.config.js
@@ -0,0 +1,52 @@
+import { getPackageJSON, getBaseRollupPlugins, resolvePath } from './utils'
+import generatePackageJson from 'rollup-plugin-generate-package-json'
+
+const { name, module } = getPackageJSON('react')
+// react 包的路径
+const reactPath = resolvePath(name)
+// react 产物路径
+const builtReactPath = resolvePath(name, true)
+
+export default [
+  // react
+  {
+    input: `${reactPath}/${module}`,
+    output: {
+      file: `${builtReactPath}/index.js`,
+      name: 'index.js',
+      format: 'umd',
+    },
+    plugins: [
+      ...getBaseRollupPlugins(),
+      generatePackageJson({
+        inputFolder: reactPath,
+        outputFolder: builtReactPath,
+        baseContents: ({ name, description, version }) => ({
+          name,
+          description,
+          version,
+          main: 'index.js',
+        }),
+      }),
+    ],
+  },
+  // jsx-runtime
+  {
+    input: `${reactPath}/src/jsx.ts`,
+    output: [
+      // jsx-runtime
+      {
+        file: `${builtReactPath}/jsx-runtime.js`,
+        name: 'jsx-runtime.js',
+        format: 'umd',
+      },
+      // jsx-dev-runtime
+      {
+        file: `${builtReactPath}/jsx-dev-runtime.js`,
+        name: 'jsx-dev-runtime.js',
+        format: 'umd',
+      },
+    ],
+    plugins: getBaseRollupPlugins(),
+  },
+]
---
+++ scripts/rollup/utils.js
@@ -0,0 +1,22 @@
+import path from 'path'
+import fs from 'fs'
+import ts from 'rollup-plugin-typescript2'
+import cjs from '@rollup/plugin-commonjs'
+
+const packages = path.resolve(__dirname, '../../packages')
+const dist = path.resolve(__dirname, '../../dist/node_modules')
+
+export function resolvePath(name, isDist) {
+  return isDist ? `${dist}/${name}` : `${packages}/${name}`
+}
+
+export function getPackageJSON(name) {
+  const path = `${resolvePath(name)}/package.json`
+  const info = fs.readFileSync(path, { encoding: 'utf-8' })
+  return JSON.parse(info)
+}
+
+export function getBaseRollupPlugins(props = {}) {
+  const { typescriptConfig = {} } = props
+  return [cjs(), ts(typescriptConfig)]
+}
