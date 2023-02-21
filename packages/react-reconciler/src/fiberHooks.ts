a779b5320be181894b124642bc6087888dad82a9
kizuner-bonely <1003306162@qq.com>
2023-02-22 01:18:02 +0800

    feat: 完成hook基本架构

--- packages/react-reconciler/src/fiberHooks.ts
+++ packages/react-reconciler/src/fiberHooks.ts
@@ -1,9 +1,35 @@
-import { ReactElement } from './../../shared/ReactTypes'
+import internals from 'shared/internals'
 import { FiberNode, FunctionComponentType } from './fiber'
 
+const { currentDispatcher } = internals
+
+let currentlyRenderingFiber: FiberNode | null = null
+// eslint-disable-next-line prefer-const
+let workInProgressHook: Hook | null = null
+
+interface Hook {
+  memoizedState: any
+  updateQueue: unknown
+  next: Hook | null
+}
+
 export function renderWithHooks(wip: FiberNode) {
+  currentlyRenderingFiber = wip
+  wip.memoizedState = null
+
+  const current = wip.alternate
+
+  if (current) {
+    // TODO update
+  } else {
+    // TODO mount
+    // currentDispatcher.current
+  }
+
   const Component = wip.type
   const props = wip.pendingProps
   const children = (Component as FunctionComponentType)(props)
+
+  currentlyRenderingFiber = null
   return children
 }
--- scripts/rollup/react-dom.config.js
+++ scripts/rollup/react-dom.config.js
@@ -2,14 +2,11 @@ import { getPackageJSON, getBaseRollupPlugins, resolvePath } from './utils'
 import generatePackageJson from 'rollup-plugin-generate-package-json'
 import alias from '@rollup/plugin-alias'
 
-const { name, module } = getPackageJSON('react-dom')
+const { name, module, peerDependencies } = getPackageJSON('react-dom')
 // react-dom 包的路径
 const reactDOMPath = resolvePath(name)
 // react-dom 产物路径
 const builtReactDOMPath = resolvePath(name, true)
-
-console.log('reactDOMPath', reactDOMPath)
-console.log('builtReactDOMPath', builtReactDOMPath)
 
 export default [
   {
@@ -26,6 +23,7 @@ export default [
         format: 'umd',
       },
     ],
+    external: [...Object.keys(peerDependencies)],
     plugins: [
       ...getBaseRollupPlugins(),
       alias({
